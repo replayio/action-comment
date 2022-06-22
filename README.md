19d3e08ee14bc1fbf1b6ab8baa3a30c382e5abb4
Ryan Duffy <ryan@replay.io>
2022-06-22 13:33:26 -0700

    Initial refactor from action-playwright (#1)
    * Initial impl from action-playwright
    
    * safely wrap message inputs

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# action-comment
-Comment on PRs with results of an Replay automated test run
+# `replayio/action-comment`
+
+> Comment on PRs with links to [Replay](https://replay.io) of replays and test runs
+
+## Usage
+
+```yaml
+- name: Generate Test Run ID
+  id: test-run-id
+  run: echo "::set-output name=UUID::$(uuidgen | tr A-Z a-z)"
+  shell: bash
+- uses: replayio/action-comment@main
+  with:
+    apiKey: ${{ secrets.RECORD_REPLAY_API_KEY }}
+    issue-number: ${{ github.event.pull_request.number }}
+    test-run-id: ${{ steps.test-run-id.outputs.UUID }}
+```
+
+## Arguments
+
+Required | Name | Description | Default
+-------- | ---- | ----------- | -------
+:white_check_mark: | `api-key` | The Replay API Key used to upload recordings
+:white_check_mark:  | `issue-number` | The number of the pull request to comment with failed test links | 
+&nbsp; | `recordings` | An array of recordings from the Replay CLI | 
+&nbsp; | `summary-message` | Templated message (by [@formatjs/intl](https://www.npmjs.com/package/@formatjs/intl)) for the upload summary. | `false`
+&nbsp; | `test-run-id` | UUID generated for a test run. If omitted, the test run link will not be included in the comment. |
+&nbsp; | `test-run-message` | Templated message (by [@formatjs/intl](https://www.npmjs.com/package/@formatjs/intl)) for the test run link. | `false`
---
+++ comment.js
@@ -0,0 +1,130 @@
+const fetch = require("node-fetch");
+const {createIntl} = require("@formatjs/intl");
+
+async function getWorkspaceId(apiKey) {
+  try {
+    const resp = await fetch("https://api.replay.io/v1/graphql", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${apiKey}`,
+      },
+      body: JSON.stringify({
+        variables: {},
+        query: `
+          query GetWS {
+            viewer {
+              user {
+                id
+              }
+            }
+            auth {
+              workspaces {
+                edges {
+                  node {
+                    id
+                  }
+                }
+              }
+            }
+          }
+        `,
+      }),
+    });
+
+    const json = await resp.json();
+
+    if (json.errors) {
+      throw new Error(errors[0].message);
+    } else if (!json.data) {
+      throw new Error("No data was returned");
+    } else if (json.data.user) {
+      return new Error("Unable to return a team for a user API Key");
+    }
+
+    const workspaces = json.data.auth.workspaces.edges;
+    if (workspaces.length !== 1) {
+      // This shouldn't happen because API keys belong to a single workspace
+      throw new Error("Multiple teams returned for the provided API key");
+    }
+
+    return workspaces[0].node.id;
+  } catch (e) {
+    console.log((e && e.message) || "Unexpected error retrieving team ID");
+    return null;
+  }
+}
+
+async function comment({
+  apiKey,
+  context,
+  github,
+  issue_number,
+  recordings,
+  summaryMessage,
+  testRunId,
+  testRunMessage,
+}) {
+  const {
+    repo: { owner, repo },
+  } = context;
+
+  const intl = createIntl(
+    {
+      locale: 'en',
+      messages: {
+        summaryMessage: {
+          id: "summary",
+          defaultMessage: summaryMessage
+        },
+        testRunMessage: {
+          id: "test-run",
+          defaultMessage: testRunMessage
+        },
+      },
+    }
+  )
+
+  if (!issue_number) {
+    console.log("No issue number");
+    return;
+  }
+
+  if (!recordings || recordings.length === 0) {
+    console.log("No recordings created");
+    return;
+  }
+
+
+  let formattedTestRunMessage = "";
+  if (apiKey && testRunId) {
+    const workspaceId = await getWorkspaceId(apiKey);
+    if (workspaceId) {
+      formattedTestRunMessage = intl.formatMessage(intl.messages.testRunMessage, {
+        link: `https://app.replay.io/team/${workspaceId}/test-run/${testRunId}`
+      });
+    }
+  }
+
+  const body = `# [![logo](https://static.replay.io/images/logo-horizontal-small-light.svg)](https://app.replay.io)
+
+  :wave: Hey there! ${intl.formatMessage(intl.messages.summaryMessage, {count: recordings.length}).trim()}
+  
+  ${formattedTestRunMessage}
+  
+  ${recordings
+    .map(
+      ({ id, metadata: { title } }) =>
+        `* [${title || id}](https://app.replay.io/recording/${id})`
+    )
+    .join("\n")}`;
+
+  return github.rest.issues.createComment({
+    issue_number,
+    owner,
+    repo,
+    body,
+  });
+}
+
+module.exports = comment;
