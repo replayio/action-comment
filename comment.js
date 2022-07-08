96b2c1f95ce0589dda4734e1a6914f7524135cd8
Ryan Duffy <ryan@replay.io>
2022-07-08 10:57:33 -0700

    clean up

--- comment.js
+++ comment.js
@@ -99,19 +99,13 @@ async function comment({
   let formattedTestRunMessage = "";
   if (apiKey && testRunId) {
     const workspaceId = await getWorkspaceId(apiKey);
-    console.log(">>>>", 1);
     if (workspaceId) {
-      console.log(">>>>", 2);
       formattedTestRunMessage = intl.formatMessage(intl.messages.testRunMessage, {
         link: `https://app.replay.io/team/${workspaceId}/runs/${testRunId}`
       });
     }
   }
 
-
-  console.log({apiKey});
-  console.log({testRunId});
-  console.log({formattedTestRunMessage});
 
   // const commitTitle = recordings[0].metadata.source.commit.title;
   const commitId = recordings[0].metadata.source.commit.id;
